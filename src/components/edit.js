0a1b83e5f7c6fcee75d9cb3a1fbc865e8f6ce9e2
mono <mohamadnobakht@gmail.com>
2021-08-19 03:10:28 +0430

    Implement reading url params

--- src/components/edit.js
+++ src/components/edit.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 export class EditComponent extends Component {
 
     render() {
-        return <div> EDIT </div>
+        return <div> EDIT {this.props.match.params.register_name} - {this.props.match.params.pk} </div>
     }
 
     componentDidMount() {}
