3363163545cdf80b812e06443aa8949c4af58041
mono <mohamadnobakht@gmail.com>
2021-08-21 01:13:47 +0430

    Implement home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,35 +1,94 @@
 import React, { Component }  from 'react';
-import { Link } from "react-router-dom";
+import { withStyles } from '@material-ui/core/styles';
+import { IconButton, ListItemSecondaryAction} from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import Link from '@material-ui/core/Link';
 import { getMainMetadata } from '../services/metadata';
-import { getListPage } from '../services/url';
+import { getListPage, getCreatePage } from '../services/url';
 import { BaseComponent } from './base';
+
+
+const pageStyle = theme => ({
+    root: {
+        width: '100%',
+        maxWidth: 700,
+        color: '#2f2d2d',
+        // backgroundColor: 'inherit',
+        // textAlign: 'center'
+    },
+});
 
 
 class PageComponent extends Component {
 
     render() {
+        const { classes } = this.props;
         return (
-            <>
-                <h2>{this.props.page.category}</h2>
-                <h3>
-                    <Link to={getListPage(this.props.page.register_name)}>
-                        {this.props.page.plural_name}
-                    </Link>
-                </h3>
-            </>
+            <div className={classes.root}>
+                <Link component='a' href={getListPage(this.props.page.register_name)}>
+                    <ListItem button>
+                        <ListItemText primary={this.props.page.plural_name} />
+                        { this.props.page.has_create_permission &&
+                            <ListItemSecondaryAction>
+                                <Link component='a' href={getCreatePage(this.props.page.register_name)}>
+                                    <IconButton edge='end'>
+                                        <AddIcon/>
+                                    </IconButton>
+                                </Link>
+                            </ListItemSecondaryAction>
+                        }
+                    </ListItem>
+                </Link>
+                <Divider/>
+            </div>
         )
     }
 }
+
+const MaterialPageComponent = withStyles(pageStyle, { withTheme: true })(PageComponent)
+
+
+const pagesStyle = theme => ({
+    root2: {
+        width: '100%',
+        maxWidth: 700,
+        color: '#2f2d2d',
+        // backgroundColor: '#405998',
+        textAlign: 'left'
+    },
+});
 
 
 class PagesComponent extends Component {
 
     render() {
-        return this.props.pages.map(page => {
-            return <PageComponent key={page.register_name} page={page} />
-        })
+        const { classes } = this.props;
+        return(
+            this.props.pages.map(category => {
+                let category_name = Object.keys(category)[0];
+                return (
+                    <div className={classes.root2}>
+                        <h4>{category_name}</h4>
+                        <Divider/>
+                        <List component='nav'>
+                            {
+                                category[category_name].map(page => {
+                                    return <MaterialPageComponent key={page.register_name} page={page}/>
+                                })
+                            }
+                        </List>
+                    </div>
+                )
+            })
+        )
     }
 }
+
+const MaterialPagesComponent = withStyles(pagesStyle, { withTheme: true })(PagesComponent)
 
 
 export class HomeComponent extends BaseComponent {
@@ -53,7 +112,7 @@ export class HomeComponent extends BaseComponent {
         if (this.state.isMetadataLoaded)
         {
             return (
-                <PagesComponent pages={this.state.metadata}/>
+                <MaterialPagesComponent pages={this.state.metadata}/>
             )
         }
         else {
