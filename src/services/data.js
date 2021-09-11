1ccb7315a8966a36360fce263506aeb23098a71f
mono <mohamadnobakht@gmail.com>
2021-09-12 02:01:42 +0430

    Implement inclusive search

--- src/services/data.js
+++ src/services/data.js
@@ -3,7 +3,7 @@ import {CONFIGS} from '../core/configs';
 
 export function find(registerName, page=null,
                      pageSize=null, orderBy=null,
-                     orderDirection='asc') {
+                     orderDirection='asc', search=null) {
     let url = `${CONFIGS.admin_api}${registerName}/?${CONFIGS.page_key}=${page}&${CONFIGS.page_size_key}=${pageSize}`;
     if (orderBy) {
         let name = orderBy.field;
@@ -15,6 +15,9 @@ export function find(registerName, page=null,
             name = `${sign}${name}`;
             url = `${url}&${CONFIGS.ordering_key}=${name}`;
         }
+    }
+    if (search) {
+        url = `${url}&${CONFIGS.query_param}=${search}`
     }
 
     return fetch(url).then(response => response.json());
