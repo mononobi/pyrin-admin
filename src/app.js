5780e56d8088d99cd74a228a299e5111a578b9b9
mono <mohamadnobakht@gmail.com>
2021-10-03 02:34:58 +0330

    Load configs on app component mount

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import { CONFIGS } from './core/configs';
+import { CONFIGS, loadConfigs } from './core/configs';
 import { ROUTES } from './routing/routes';
 import { BaseComponent } from './components/base/base/base';
 
@@ -8,21 +8,26 @@ import { BaseComponent } from './components/base/base/base';
 export class App extends BaseComponent {
 
   state = {
-    authenticated: true
+      authenticated: true
+  }
+
+  _componentDidMount() {
+      loadConfigs();
   }
 
   _render() {
       return (
-        <BrowserRouter>
-          <Switch>
-            <Route key='-1' exact path='/'>
-              <Redirect to={CONFIGS.panel_home_path} />
-            </Route>
-            {ROUTES.map((route, index) => (
-                <Route key={index.toString()} exact={route.exact} path={route.path} component={route.component}/>
-            ))}
-          </Switch>
-        </BrowserRouter>
-    )
-  }
+            <BrowserRouter>
+                <Switch>
+                    <Route key='-1' exact path='/'>
+                        <Redirect to={CONFIGS.panel_home_path}/>
+                    </Route>
+                    {ROUTES.map((route, index) => (
+                        <Route key={index.toString()} exact={route.exact} path={route.path}
+                               component={route.component}/>
+                    ))}
+                </Switch>
+            </BrowserRouter>
+        )
+    }
 }
