06fe481a5a76972b2e590f0bfa5ce74231c3672a
mono <mohamadnobakht@gmail.com>
2021-10-06 01:05:41 +0330

    Implement select dialog

--- src/components/controls/dialogs/select.js
+++ src/components/controls/dialogs/select.js
@@ -1,16 +1,12 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
-import ListItemText from '@mui/material/ListItemText';
-import ListItem from '@mui/material/ListItem';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import CloseIcon from '@mui/icons-material/Close';
 import Slide from '@mui/material/Slide';
+import { ListComponent } from '../../pages/list/list';
 
 
 const Transition = React.forwardRef(function Transition(props, ref) {
@@ -18,7 +14,7 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 });
 
 
-export default function FullScreenDialog() {
+export const SelectDialog = props => {
     const [open, setOpen] = React.useState(false);
 
     const handleClickOpen = () => {
@@ -40,37 +36,19 @@ export default function FullScreenDialog() {
                 onClose={handleClose}
                 TransitionComponent={Transition}
             >
-                <AppBar sx={{ position: 'relative' }}>
+                <AppBar sx={{position: 'relative'}}>
                     <Toolbar>
                         <IconButton
-                            edge='start'
+                            edge='end'
                             color='inherit'
                             onClick={handleClose}
-                            aria-label='close'
                         >
-                            <CloseIcon />
+                            <CloseIcon/>
                         </IconButton>
-                        <Typography sx={{ ml: 2, flex: 1 }} variant='h6' component='div'>
-                            Sound
-                        </Typography>
-                        <Button autoFocus color='inherit' onClick={handleClose}>
-                            save
-                        </Button>
                     </Toolbar>
                 </AppBar>
-                <List>
-                    <ListItem button>
-                        <ListItemText primary='Phone ringtone' secondary='Titania' />
-                    </ListItem>
-                    <Divider />
-                    <ListItem button>
-                        <ListItemText
-                            primary='Default notification ringtone'
-                            secondary='Tethys'
-                        />
-                    </ListItem>
-                </List>
+                <ListComponent forSelect={true} setSelectedPK={props.setSelectedPK}/>
             </Dialog>
         </div>
     );
-}
+};
