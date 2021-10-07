89ab5c4a6107bd90a41eec4f98fbd7d83b285e44
mono <mohamadnobakht@gmail.com>
2021-10-08 01:04:26 +0330

    Read default fields value from metadata

--- src/components/forms/create.js
+++ src/components/forms/create.js
@@ -14,7 +14,10 @@ export class CreateForm extends FormBase {
         for (let i = 0; i < this.props.dataFields.length; i++) {
             let name = this.props.dataFields[i].field;
             let info = this.props.dataFieldsDict[name]
-            if (info && info.form_field_type === ServerFormFieldTypeEnum.BOOLEAN) {
+            if (info && !this._isNull(info.default)) {
+                result[name] = info.default;
+            }
+            else if (info && info.form_field_type === ServerFormFieldTypeEnum.BOOLEAN) {
                 result[name] = this.CHECKBOX_DEFAULT;
             }
             else {
