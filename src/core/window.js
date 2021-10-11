ad7faf62a561d29f7e3f9eb56c3b5159422e049c
mono <m.nobakht@khodro45.com>
2021-10-11 23:21:07 +0330

    Enhance auto height detection

--- src/core/window.js
+++ src/core/window.js
@@ -1,7 +1,27 @@
+const BASE_RATIO = 1.6;
+const BASE_HEIGHT = 1200;
+const BASE_WIDTH = 1920;
+
 export function getMaxHeight(reducer, forSelectReducer, forSelect) {
     let height = window.screen.availHeight;
+    let factor = window.screen.height / BASE_HEIGHT;
+    reducer = reducer / factor;
+    forSelectReducer = forSelectReducer / factor;
+
+    let result = 0;
     if (!forSelect) {
-        return `${height - (reducer * height)}px`;
+        result = height - (reducer * height);
     }
-    return `${height - (forSelectReducer * height)}px`;
+    else {
+        result = height - (forSelectReducer * height);
+    }
+
+    if (factor < 0.8) {
+        result = result - 12;
+    }
+    else if (factor > 1.2) {
+        result = result + 12;
+    }
+
+    return `${result}px`;
 }
